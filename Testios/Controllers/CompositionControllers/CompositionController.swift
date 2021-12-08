6ac4058ca59bd5e8f15a725f6d0e56df1e8d8461
Chad Garrett <cgarrett@retrorabbit.co.za>
2021-12-08 22:30:22 +0200

    Logic controller example

--- Testios/Controllers/CompositionControllers/CompositionController.swift
+++ Testios/Controllers/CompositionControllers/CompositionController.swift
@@ -12,19 +12,25 @@ import UIKit
 /// Lightweight controller that has most functionallity added via view controller composition
 final class CompositionController: AppViewController {
 
-    private struct Item {
+    internal struct Item {
         var key: Int64
         var text: String
     }
 
+    internal enum State {
+        case loading
+        case presenting(Item)
+        case error
+    }
+
     // MARK: - Properties
 
-    private lazy var endpoint = MockEndpoint<Item>()
+    private lazy var logicController = CompositionLogicController()
 
     // MARK: Properties
 
-    private var item: Item? {
-        didSet { self.itemDidUpdate() }
+    private var state: State = .loading {
+        didSet { self.stateDidUpdate() }
     }
 
     // MARK: - Setup
@@ -58,21 +64,43 @@ final class CompositionController: AppViewController {
 
         let loadingController = LoadingController()
         self.add(loadingController)
-
-        self.endpoint.fetchData(.init(key: 1, text: "Item 1")) { result in
+        self.logicController.load { state in
             loadingController.remove()
-            if let item = try? result.get() {
-                self.item = item
-            }
+            self.state = state
         }
     }
 
-    private func itemDidUpdate() {
-        guard let item = item
-        else { return }
+    private func stateDidUpdate() {
+        switch self.state {
+        case .presenting(let item): self.render(item: item)
+        case .error: self.handleError()
+        case .loading: break
+        }
+    }
 
+    private func render(item: Item) {
         self.onMain { [weak self] in
             self?.lblItemView.attributedText = .init(string: item.text, attributes: Style.callout)
+        }
+    }
+
+    private func handleError() {
+
+    }
+}
+
+class CompositionLogicController {
+    typealias Handler = (CompositionController.State) -> Void
+
+    private lazy var endpoint = MockEndpoint<CompositionController.Item>()
+
+    func load(then handler: @escaping Handler) {
+        self.endpoint.fetchData(.init(key: 1, text: "Item 1")) { result in
+            if let item = try? result.get() {
+                handler(.presenting(item))
+            } else {
+                handler(.error)
+            }
         }
     }
 }
