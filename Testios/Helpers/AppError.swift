cf8fd1bfd0f562353da0444e7fdec769d8d13376
Chad Garrett <cgarrett@retrorabbit.co.za>
2021-12-08 22:08:16 +0200

    srn Added an AppError type for use in mock endpoint

---
+++ Testios/Helpers/AppError.swift
@@ -0,0 +1,13 @@
+//
+//  AppError.swift
+//  Testios
+//
+//  Created by Chad Garrett on 2021/12/08.
+//  Copyright © 2021 Personal. All rights reserved.
+//
+
+public enum AppError: Error {
+    case unknown
+
+    case apiError
+}
